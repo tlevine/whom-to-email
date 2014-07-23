8a9e7d629bce1c3057038a3771f072da7de2ddf4
Thomas Levine <_@thomaslevine.com>
2014-07-23 07:58:23 -0400

    weights

--- sample.R
+++ sample.R
@@ -4,6 +4,37 @@
 counts.file <- 'data/2014-07-23/counts' # argv[1] would be better
 people.file <- '~/.mutt/aliases/people'
 
+# The weighting function
+weights <- function(counts) {
+    sqrt(counts$personal.old) - counts$personal.new
+}
+
+# A plot that can be helpful for tuning the weight function
+tune.weights <- function() {
+  library(ggplot2)
+  counts <- counts
+
+  p1 <- ggplot(counts) +
+    aes(x = personal.old, y = personal.new, label = address) +
+    geom_text() + scale_x_log10('Old') + scale_y_log10('New')
+
+  counts$recent.contact <- factor(counts$personal.new == 0, levels = c(TRUE,FALSE))
+  levels(counts$recent.contact) <- c('Yes','No')
+
+  p2 <- ggplot(counts) +
+    aes(x = everything, y = personal.old, color = recent.contact, label = address) +
+    scale_color_discrete('Recent email?') +
+    geom_text() + scale_x_log10('Everyone') + scale_y_log10('Me')
+
+  p3 <- ggplot(counts) +
+    aes(x = personal.old, y = weights(counts), color = recent.contact, label = address) +
+    scale_color_discrete('Recent email?') +
+    geom_text() + scale_x_log10('Old emails') + scale_y_continuous('Sampling weight')
+
+  # list(p1 = p1, p2 = p2)
+  p3
+}
+
 # Open the files.
 counts <- read.csv(counts.file, sep = '\t', stringsAsFactors = FALSE)
 connection <- file(people.file)
@@ -15,7 +46,7 @@ people$name <- sub(' .*', '', sub('alias ', '', people$raw))
 people$address <- sub('.* <?([^ >]+)>?$', '\\1', people$raw)
 
 # Weight the probabilities.
-prob.numerator <- pmax(0, sqrt(counts$personal.old) - counts$personal.new)
+prob.numerator <- pmax(0, weights(counts))
 
 # Take the sample, using the file as a seed.
 set.seed(sum(counts[-1]))
