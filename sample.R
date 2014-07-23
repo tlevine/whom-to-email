c67e1c644276ad6d96e818a3ac5615560550b7ec
Thomas Levine <_@thomaslevine.com>
2014-07-23 08:18:42 -0400

    wow this kind of works

--- sample.R
+++ sample.R
@@ -6,33 +6,7 @@ people.file <- '~/.mutt/aliases/people'
 
 # The weighting function
 weights <- function(counts) {
-    pmax(0, (counts$personal.old)^(1/4) - (counts$personal.new)^(1/2) )
-}
-
-# A plot that can be helpful for tuning the weight function
-tune.weights <- function() {
-  library(ggplot2)
-  counts <- counts
-
-  p1 <- ggplot(counts) +
-    aes(x = personal.old, y = personal.new, label = address) +
-    geom_text() + scale_x_log10('Old') + scale_y_log10('New')
-
-  counts$recent.contact <- factor(counts$personal.new > 0, levels = c(TRUE,FALSE))
-  levels(counts$recent.contact) <- c('Yes','No')
-
-  p2 <- ggplot(counts) +
-    aes(x = everything, y = personal.old, color = recent.contact, label = address) +
-    scale_color_discrete('Recent email?') +
-    geom_text() + scale_x_log10('Everyone') + scale_y_log10('Me')
-
-  p3 <- ggplot(counts) +
-    aes(x = personal.old, y = weights(counts), color = recent.contact, label = address) +
-    scale_color_discrete('Recent email?') +
-    geom_text() + scale_x_log10('Old emails') + scale_y_continuous('Sampling weight')
-
-  # list(p1 = p1, p2 = p2)
-  p3
+    sqrt(pmax(0, (counts$personal.old - counts$personal.new) / sqrt(counts$everything + 1)))
 }
 
 # Open the files.
@@ -54,6 +28,20 @@ addresses <- sample(counts$address, 8, prob = prob.numerator)
 
 # Emit the result.
 pattern <- paste(addresses, collapse = '|')
+
+# A plot that can be helpful for tuning the weight function
+if (require(ggplot2)) {
+  counts <- counts
+
+  counts$recent.contact <- factor(counts$personal.new > 0, levels = c(TRUE,FALSE))
+  levels(counts$recent.contact) <- c('Yes','No')
+
+  p <- ggplot(counts) +
+    aes(x = personal.old, y = weights(counts), color = recent.contact, label = address) +
+    scale_color_discrete('Recent email?') +
+    geom_text() + scale_x_log10('Old emails') + scale_y_continuous('Sampling weight')
+  ggsave(sub('counts','weights.pdf', counts.file), p, width = 8.5, height = 14, units = 'in')
+}
 
 cat('Email the first of these people whom you haven\'t seen recently.\n\n')
 cat(paste0(paste(people[grep(pattern, people$address),'name'], collapse = '\n'), '\n'))
