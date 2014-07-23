c4baa7f45d2dce13d16725d8a6e2a353d3bcee95
Thomas Levine <_@thomaslevine.com>
2014-07-23 07:08:46 -0400

    sampling

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 directory := data/$(shell date --rfc-3339=date)
 
+	
+
 $(directory)/counts: $(directory)/names
 	printf 'address\ttotal\tpersonal\n' > $(directory)/counts
 	while read address; do printf "$$address\t" >> $(directory)/counts && notmuch count "$$address" | tr '\n' '\t' >> $(directory)/counts && notmuch count "to:$$address and (from:thomas or from:levine)" >> $(directory)/counts; done < $(directory)/names
--- sample.R
+++ sample.R
@@ -1,15 +1,20 @@
+#!/usr/bin/env Rscript
+
 # Inputs: The file of counts, and the number of addresses to sample
 filename <- 'data/2014-07-23/counts' # argv[1] would be better
-size <- 1
+size <- 10
 
 # Open the file.
 counts <- read.csv(filename, sep = '\t', stringsAsFactors = FALSE)
 
-# Weight the probabilities.
-counts$prob.numerator <- sqrt(counts$personal)
 
-# Take the sample.
-address <- sample(counts$address, size, prob = counts$prob.numerator)
+# Weight the probabilities.
+prob.numerator <- sqrt(counts$personal)
+
+# Take the sample, using the file as a seed.
+seed <- sum(counts[-1])
+set.seed(seed)
+address <- sample(counts$address, size, prob = prob.numerator)
 
 # Emit the result.
 cat(address, '\n')
