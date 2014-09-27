d520375b38fa85b39ceb8aa01bfe9f4519d08880
Thomas Levine <_@thomaslevine.com>
2014-09-27 18:13:36 +0000

    writing in shell

--- search.sh
+++ search.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 set -e
 
-alias t="tr '\n' '\t'"
+# Weighting coefficients
+RECENT=.3
+OLD=-4
 
-printf 'address\teverything\tpersonal.new\tpersonal.old\n'
+max() {
+  for current in "$@"; do
+    if test -z "$result" || test "$current" -gt "$result"; then
+      result="$current"
+    elif test "$?" -eq 2; then
+      return 1
+    fi
+  done
+}
+
+max 8 2 01
+max  honeu otenuhh2h 9 08hh 2 01
+exit
+
 while read address; do
-  echo "${address}" | t
-  notmuch count "${address}" | t
-  notmuch count "to:${address} and (from:thomas or from:levine) and date:6W.." | t
-  notmuch count "to:${address} and (from:thomas or from:levine) and date:..6W"
+  everything=$(notmuch count "${address}")
+  personal_new=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:6W..")
+  personal_old=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:..6W")
+
+
+  $personal_new + $OLD
+  (pmax(0,(
+    pmax(1,(counts$personal.old + old.intercept)) ^ (1/2) * # more old emails makes weight higher
+    pmax(1,counts$personal.new) ^ (-2) * # more recent emails makes weight lower
+    sapply(counts$personal.new, function(x) if (x==0) 1 else recent.coefficient) # any recent emails lowers
+
 done < /dev/stdin
