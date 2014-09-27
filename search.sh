e079d15c9e1a2b12bc79a46f919105a4011e69d3
Thomas Levine <_@thomaslevine.com>
2014-09-27 18:27:18 +0000

    hmmm

--- search.sh
+++ search.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+# Because we don't have floats
+PRECISION=100000
+
 # Weighting coefficients
-RECENT=.3
+RECENT=$(($PRECISION * 3 / 10))
 OLD=-4
 
 max() {
@@ -21,13 +24,14 @@ while read address; do
   personal_old=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:..6W")
 
 
-  x1=$(max 1 $(( ($personal_old + $OLD) ** (1/2) )))
-  x2=$(max 1 $(( $personal_new ** (-2) )))
+  x1=$(max $PRECISION $(( ($PRECISION * ($personal_old + $OLD)) ** (1/2) )))
+  x2=$(max $PRECISION $(( $PRECISION / ($personal_new ** (2)) )))
   if test $personal_new -eq 0; then
-    x3=1
+    x3=$PRECISION
   else
-    x3=$RECENT
+    echo $RECENT
+    x3=$(($PRECISION * $RECENT))
   fi
 
-  echo "${address}", $(max 0 $(($x1 * $x2 * $x3)))
+  echo "${address}", $(max 0 $(( ($x1 * $x2 * $x3) / ($PRECISION ** 3) )) )
 done < /dev/stdin
