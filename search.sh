41b90ba12e7d76628e4c1dc252176e7949bc33f4
Thomas Levine <_@thomaslevine.com>
2014-09-27 18:19:24 +0000

    rewrote in shell

--- search.sh
+++ search.sh
@@ -15,20 +15,19 @@ max() {
   done
 }
 
-max 8 2 01
-max  honeu otenuhh2h 9 08hh 2 01
-exit
-
 while read address; do
   everything=$(notmuch count "${address}")
   personal_new=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:6W..")
   personal_old=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:..6W")
 
 
-  $personal_new + $OLD
-  (pmax(0,(
-    pmax(1,(counts$personal.old + old.intercept)) ^ (1/2) * # more old emails makes weight higher
-    pmax(1,counts$personal.new) ^ (-2) * # more recent emails makes weight lower
-    sapply(counts$personal.new, function(x) if (x==0) 1 else recent.coefficient) # any recent emails lowers
+  x1=$(max 1 $(( ($personal_old + $OLD) ** (1/2) )))
+  x2=$(max 1 $(( $personal_new ** (-2) )))
+  if test $personal_new -eq 0; then
+    x3=1
+  else
+    x3=$RECENT
+  fi
 
+  echo "${address}", $(max 0 $(($x1 * $x2 * $x3)))
 done < /dev/stdin
