06a719ffad05129213f3ed96d3257316dd8ed57d
Thomas Levine <_@thomaslevine.com>
2014-10-11 13:57:50 +0000

    -

--- search.sh
+++ search.sh
@@ -7,18 +7,21 @@ OLD=-4
 
 max() {
   for current in "$@"; do
-    if test -z "$result" || test 1 -eq $(bc "$current > $result"); then
+    if test -z "$result" || test 1 -eq $(echo "${current} > ${result}" | bc); then
       result="$current"
     elif test "$?" -eq 2; then
       return 1
     fi
   done
+  echo "${result}"
 }
 
 while read address; do
   everything=$(notmuch count "${address}")
   personal_new=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:6W..")
   personal_old=$(notmuch count "to:${address} and (from:thomas or from:levine) and date:..6W")
+  echo $everything, $personal_new, $personal_old
+  max 3 2 4 
 
   x1=$(echo "sqrt($personal_old + $OLD)" | bc -l)
   x2=$(max 1 $(echo "1 / ($personal_new) ^ (2)" | bc -l)) # more recent emails makes weight lower
